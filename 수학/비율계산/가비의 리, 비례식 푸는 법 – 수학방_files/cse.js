50c6f02b2cfa590a207eda63e492d56e1d44cbf4
khh <khh@omnicns.com>
2018-02-19 07:28:31 +0900

    math

---
+++ 수학/비율계산/가비의 리, 비례식 푸는 법 – 수학방_files/cse.js
@@ -0,0 +1,34 @@
+(function(opts_){window.__gcse=window.__gcse||{};window.__gcse.ct=(new Date).getTime();window.__gcse.scb=function(){var a=window.__gcse;a.plainStyle&&delete opts_.rawCss;google.search.cse.element.init(opts_)&&("explicit"!=a.parsetags?"complete"==document.readyState||"interactive"==document.readyState?(google.search.cse.element.go(),a.callback&&a.callback()):google.setOnLoadCallback(function(){google.search.cse.element.go();a.callback&&a.callback()},!0):a.callback&&a.callback())};
+var b=document.createElement("script"),c=opts_.protocol+"://"+opts_.uds+"/"+(opts_.loaderPath||"jsapi")+"?autoload=",d=encodeURIComponent,e='{"name":"search","version":"1.0","callback":"__gcse.scb"',f=window.__gcse;if(!f||!f.plainStyle)var g=opts_.protocol+(opts_.uiOptions&&opts_.uiOptions.enableMobileLayout?"://www.google.com/cse/static/style/look/mobile/":"://www.google.com/cse/static/style/look/")+opts_.theme.toLowerCase().replace("v2_","v2/")+".css",e=e+(',"style":"'+g+'"');
+opts_.language&&(e+=',"language":"'+opts_.language+'"');var h=c+d('{"modules":['+(e+"}")+"]}");opts_.callback&&(h=h+"&callback="+opts_.callback);b.src=h+"";b.type="text/javascript";document.getElementsByTagName("head")[0].appendChild(b);
+})({
+  "cx": "partner-pub-0495028602223778:3065460798",
+  "language": "ko",
+  "theme": "DEFAULT",
+  "uiOptions": {
+    "resultsUrl": "http://mathbang.net/search",
+    "enableAutoComplete": true,
+    "enableImageSearch": false,
+    "imageSearchLayout": "popup",
+    "resultSetSize": "filtered_cse",
+    "gaQueryParameter": "search",
+    "gaCategoryParameter": "",
+    "enableOrderBy": false,
+    "orderByOptions": [],
+    "overlayResults": false,
+    "queryParameterName": "search",
+    "enableHistory": true,
+    "enableMobileLayout": false,
+    "numTopRefinements": -1,
+    "showBookmarkLink": false,
+    "isSitesearch": false,
+    "enableSpeech": false,
+    "enableRichSnippets": true
+  },
+  "protocol": "http",
+  "uds": "www.google.com",
+  "rawCss": ".gsc-control-cse{font-family:Times, serif;border-color:#FFFFFF;background-color:#FFFFFF}.gsc-control-cse .gsc-table-result{font-family:Times, serif}input.gsc-input{border-color:#2DB400}input.gsc-search-button{border-color:#2DB400;background-color:#2DB400}.gsc-tabHeader.gsc-tabhInactive{border-color:#FFFFFF;background-color:#FFFFFF}.gsc-tabHeader.gsc-tabhActive{border-top-color:#FFFFFF;border-left-color:#FFFFFF;border-right-color:#FFFFFF;background-color:#FFFFFF}.gsc-tabsArea{border-color:#FFFFFF;}.gsc-webResult.gsc-result,.gsc-results .gsc-imageResult{border-color:#FFFFFF;background-color:#FFFFFF}.gsc-webResult.gsc-result:hover,.gsc-imageResult:hover{border-color:#FFFFFF;background-color:#FFFFFF}.gsc-webResult.gsc-result.gsc-promotion:hover{border-color:#FFFFFF;background-color:#FFFFFF}.gs-webResult.gs-result a.gs-title:link,.gs-webResult.gs-result a.gs-title:link b,.gs-imageResult a.gs-title:link,.gs-imageResult a.gs-title:link b{color:#0077FF}.gs-webResult.gs-result a.gs-title:visited,.gs-webResult.gs-result a.gs-title:visited b,.gs-imageResult a.gs-title:visited,.gs-imageResult a.gs-title:visited b{color:#663399}.gs-webResult.gs-result a.gs-title:hover,.gs-webResult.gs-result a.gs-title:hover b,.gs-imageResult a.gs-title:hover,.gs-imageResult a.gs-title:hover b{color:#FF0000}.gs-webResult.gs-result a.gs-title:active,.gs-webResult.gs-result a.gs-title:active b,.gs-imageResult a.gs-title:active,.gs-imageResult a.gs-title:active b{color:#0077FF}.gsc-cursor-page{color:#0077FF}a.gsc-trailing-more-results:link{color:#0077FF}.gs-webResult .gs-snippet,.gs-imageResult .gs-snippet,.gs-fileFormatType{color:#555555}.gs-webResult div.gs-visibleUrl,.gs-imageResult div.gs-visibleUrl{color:#008000}.gs-webResult div.gs-visibleUrl-short{color:#008000}.gs-webResult div.gs-visibleUrl-short{display:none}.gs-webResult div.gs-visibleUrl-long{display:block}.gs-promotion div.gs-visibleUrl-short{display:none}.gs-promotion div.gs-visibleUrl-long{display:block}.gsc-cursor-box{border-color:#FFFFFF}.gsc-results .gsc-cursor-box .gsc-cursor-page{border-color:#FFFFFF;background-color:#FFFFFF;color:#0077FF}.gsc-results .gsc-cursor-box .gsc-cursor-current-page{border-color:#FFFFFF;background-color:#FFFFFF;color:#663399}.gsc-webResult.gsc-result.gsc-promotion{border-color:#FFFFFF;background-color:#FFFFFF}.gsc-completion-title{color:#0077FF}.gsc-completion-snippet{color:#555555}.gs-promotion a.gs-title:link,.gs-promotion a.gs-title:link *,.gs-promotion .gs-snippet a:link{color:#0000FF}.gs-promotion a.gs-title:visited,.gs-promotion a.gs-title:visited *,.gs-promotion .gs-snippet a:visited{color:#0000FF}.gs-promotion a.gs-title:hover,.gs-promotion a.gs-title:hover *,.gs-promotion .gs-snippet a:hover{color:#FF0000}.gs-promotion a.gs-title:active,.gs-promotion a.gs-title:active *,.gs-promotion .gs-snippet a:active{color:#0000FF}.gs-promotion .gs-snippet,.gs-promotion .gs-title .gs-promotion-title-right,.gs-promotion .gs-title .gs-promotion-title-right *{color:#333333}.gs-promotion .gs-visibleUrl,.gs-promotion .gs-visibleUrl-short{color:#008000}",
+  "loaderPath": "jsapi",
+  "cse_token": "AOdTmaB1HnPz2ZFFnWpXnefLysmarYSs9g:1518753900916",
+  "isHostedPage": false
+});
